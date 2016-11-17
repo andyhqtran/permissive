b51bdeab4b248d1b2d53188603c502b5dff9bac5
Andy Tran <andyhqtran@gmail.com>
2016-11-17 15:30:47 -0800

    Added in livereload and concurrently.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import gulp from 'gulp';
+import livereload from 'gulp-livereload';
 import sass from 'gulp-sass';
 import sourcemaps from 'gulp-sourcemaps';
 
@@ -10,9 +11,12 @@ gulp.task('sass', () => {
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./assets/css'));
+    .pipe(gulp.dest('./assets/css'))
+    .pipe(livereload());
 });
 
 gulp.task('sass:watch', () => {
+  livereload.listen();
+
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
