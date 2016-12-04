2f6799f4b25128c009509104c5c3da99c3442d34
Andy Tran <andyhqtran@gmail.com>
2016-12-04 14:02:16 -0500

    Fixed eslint issues.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,14 +6,12 @@ import refresh from 'gulp-refresh';
 import sass from 'gulp-sass';
 import sourcemaps from 'gulp-sourcemaps';
 
-gulp.task('sass', () => {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./assets/css'))
-    .pipe(refresh());
-});
+gulp.task('sass', () => gulp.src('./sass/**/*.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('./assets/css'))
+  .pipe(refresh()));
 
 gulp.task('sass:watch', () => {
   refresh.listen();
