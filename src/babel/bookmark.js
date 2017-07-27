512445d2e36c37140b3805cce8522ec360b85f47
andyhqtran <hello@andytran.me>
2017-07-27 09:39:43 -0700

    Added in new Bookmark Menu.

--- src/babel/bookmark.js
+++ src/babel/bookmark.js
@@ -1,122 +1,62 @@
-function clearListItem() {
-  $('.bookmark-menu').find('.bookmark-menu__list').empty();
-}
+function getBookmarks() {
+  const data = localStorage.getItem('bookmarks');
 
-function renderListItem(post) {
-  const listItem = $(`<li class="bookmark-menu__list-item" data-bookmark-id="${post.id}" />`);
-
-  const listContent = $('<div class="bookmark-menu__list-content" />');
-  listContent.append(`<h6><a href="${post.url}">${post.title}</a></h6>`);
-  listContent.append(`<p>${post.excerpt}</p>`);
-
-  const listDelete = $('<a class="bookmark-menu__list-delete" data-bookmark-action="delete" href="#" />');
-  listDelete.append('<span class="ion ion-close-circled"></span>');
-
-  listItem.append(listContent);
-  listItem.append(listDelete);
-
-  $('.bookmark-menu__list').append(listItem);
-}
-
-function clearBookmarkData() {
-  localStorage.setItem('bookmarks', JSON.stringify({}));
-}
-
-function getBookmarkData() {
+  if (!data) {
+    localStorage.setItem('bookmarks', JSON.stringify({}));
+  }
+  // return {};
   return JSON.parse(localStorage.getItem('bookmarks'));
 }
 
-function saveBookmarkData(data) {
-  localStorage.setItem('bookmarks', JSON.stringify(data));
-}
+const BookmarkMenu = new Vue({
+  el: '.bookmark-menu',
+  delimiters: ['${', '}'],
+  data: {
+    pageId: window.post.id,
+    message: 'test',
+    bookmarks: getBookmarks(),
+  },
 
-function deleteBookmarkData(id) {
-  const data = getBookmarkData();
+  mounted() {
+    this.handleSaveButtonChange();
+  },
 
-  delete data[id];
-
-  saveBookmarkData(data);
-}
-
-$(document).ready(() => {
-  if (window.post) {
-    if (!$.isEmptyObject(getBookmarkData()[window.post.id])) {
-      if (getBookmarkData()[window.post.id].id === window.post.id) {
-        $('[data-bookmark-action="save"]').css({ background: '#FFFFFF', color: '#4C4C4C' });
-        $('[data-bookmark-action="save"]').find('span').addClass('ion-checkmark-round');
+  methods: {
+    handleSaveButtonChange() {
+      if (this.bookmarks[this.pageId]) {
+        return $('#bookmark-save')
+          .css({ background: '#FFFFFF', color: '#4C4C4C' })
+          .find('span').addClass('ion-checkmark-round');
       }
-    }
-  }
 
-  $('[data-bookmark-action="open"]').on('click', () => {
-    const posts = getBookmarkData();
+      return $('#bookmark-save')
+          .attr('style', '')
+          .find('span').removeClass('ion-checkmark-round');
+    },
+    handlePostSave() {
+      Vue.set(this.bookmarks, window.post.id, window.post);
+    },
+  },
 
-    clearListItem();
+  watch: {
+    bookmarks(val) {
+      const data = JSON.stringify(val);
 
-    if ($.isEmptyObject(posts)) {
-      const listItem = $('<li class="bookmark-menu__list-item is-null" />').text('No post have been saved.');
+      console.log('updating');
 
-      $('.bookmark-menu__list').append(listItem);
-    }
+      localStorage.setItem('bookmarks', data);
 
-    $('.bookmark-menu').toggleClass('is-active');
+      this.handleSaveButtonChange();
+    },
+  },
+});
 
-    $.each(posts, (index, post) => {
-      renderListItem(post);
-    });
-  });
+$('[data-bookmark-action="open"]').on('click', () => {
+  $('.bookmark-menu').toggleClass('is-active');
+});
 
-  $('[data-bookmark-action="clear"]').on('click', () => {
-    clearListItem();
+$('[data-bookmark-action="save"]').on('click', () => {
+  BookmarkMenu.handlePostSave();
 
-    clearBookmarkData();
-
-    if ($.isEmptyObject(getBookmarkData())) {
-      const listItem = $('<li class="bookmark-menu__list-item is-null" />').text('No post have been saved.');
-
-      $('[data-bookmark-action="save"]').attr('style', '');
-      $('[data-bookmark-action="save"]').find('span').removeClass('ion-checkmark-round');
-
-      $('.bookmark-menu__list').append(listItem);
-    }
-  });
-
-  $('.bookmark-menu').on('click', '[data-bookmark-action="delete"]', (event) => {
-    const parent = $(event.currentTarget).closest('li.bookmark-menu__list-item');
-    const postId = parent.attr('data-bookmark-id');
-
-    deleteBookmarkData(postId);
-
-    $('[data-bookmark-action="save"]').attr('style', '');
-    $('[data-bookmark-action="save"]').find('span').removeClass('ion-checkmark-round');
-
-    if ($('.bookmark-menu__list').children().length === 1) {
-      const listItem = $('<li class="bookmark-menu__list-item is-null" />').text('No post have been saved.');
-
-      $('.bookmark-menu__list').append(listItem);
-    }
-
-    parent.remove();
-  });
-
-  if ($.isEmptyObject(JSON.stringify(localStorage.getItem('bookmarks')))) {
-    localStorage.setItem('bookmarks', JSON.stringify({}));
-  }
-
-  $('[data-bookmark-action="save"]').on('click', (event) => {
-    const posts = getBookmarkData();
-
-    $(event.currentTarget).css({ background: '#FFFFFF', color: '#4C4C4C' });
-    $(event.currentTarget).find('span').addClass('ion-checkmark-round');
-
-    if (posts[window.post.id] === window.post.id) {
-      return;
-    }
-
-    if (!posts[window.post.id]) {
-      posts[window.post.id] = window.post;
-    }
-
-    saveBookmarkData(posts);
-  });
+  $('.bookmark-menu').addClass('is-active');
 });
